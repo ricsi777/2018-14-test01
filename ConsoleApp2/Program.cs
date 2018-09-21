0fc3616f12271033ba09f74e4cd8465b8535d592
Richárd Simon <richard.simon1997@gmail.com>
2018-09-21 15:54:27 +0200

    Max, min keresés és usertől való bekérés 10 számig.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,11 +8,66 @@ namespace ConsoleApp2
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static int Max(int[] tomb, out int hely)
         {
-            Console.WriteLine("Szia Tunci");
-            string szoveg = "Tuncika";
-            Console.WriteLine(szoveg);
+
+            int max = tomb[0];
+            hely = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] > max)
+                {
+                    max = tomb[i];
+                    hely = i;
+                }
+            }
+            return max;
+        }
+
+        public static int Min(int[] tomb, out int hely) {
+
+            int min = tomb[0];
+            hely = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] < min) {
+                    min = tomb[i];
+                    hely = i;
+                }
+            }
+            return min;   
+        }
+
+
+            static void Main(string[] args)
+        {
+            int[] szamok = new int[10];
+
+            Console.WriteLine("Kérem adjon meg 10 db számot!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                string keres;
+                do
+                {
+                    Console.WriteLine("Az {0} szám!",i+1);
+                    keres = Console.ReadLine();
+
+                } while (int.TryParse(keres, out szamok[i])==false);
+            }
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine(szamok[i]);
+            }
+            int helye;
+            int legnagyobb = Max(szamok, out helye);
+            int legkisebb = Min(szamok, out helye);
+            Console.WriteLine("A legnagyobb szám: {0} és a helye: {1}",legnagyobb,helye+1);
+            Console.WriteLine("A legkisebb szám: {0} és a helye: {1}",legkisebb, helye + 1);
+
+            Console.ReadKey();
         }
     }
 }
